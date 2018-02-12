92121fa12fbd87d5fa35f700e88ab39d3770958a
The Bardic Hobo <redeemedofgod@zoho.com>
2018-02-12 17:12:35 -0500

    Trying to add working database connection

--- Main.java
+++ Main.java
@@ -1,7 +1,47 @@
-# Just a filler file to show this project as java
+//Just a filler file to show this project as java
+
+import java.sql.*;
+import javax.sql.*;
 
 public class Main {
+
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost/";
+
+	static final String USER = "root";
+	static final String PASS = null;	
+	
   public static void main(String[] args) {
+	
+	Connection conn = null;
+	Statement stmt = null;
+	try {
+		Class.forName("com.mysql.jdbc.Driver");
+
+		conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+		stmt = conn.createStatement();
+
+		String sql = "CREATE DATABASE PREDICTOR";
+		stmt.executeUpdate(sql);
+	}catch(SQLException se){
+		se.printStackTrace();
+	}catch(Exception e){
+		e.printStackTrace();
+	}finally{
+		try{
+			if(stmt!=null)
+				stmt.close();
+		}catch(SQLException se2){
+		}
+		try{
+			if(conn!=null)
+				conn.close();
+		}catch(SQLException se){
+			se.printStackTrace();
+		}
+	}
+
     System.out.println("Hello World");
   }
 }
