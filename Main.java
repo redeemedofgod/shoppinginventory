7caf99caa6aeb934912b205d304e99443d79323d
The Bardic Hobo <redeemedofgod@zoho.com>
2018-02-14 23:44:09 -0500

    Got the db connection working. Change the un and pw to your mysql db

--- Main.java
+++ Main.java
@@ -1,47 +1,29 @@
 //Just a filler file to show this project as java
 
 import java.sql.*;
-import javax.sql.*;
+import java.lang.*;
 
 public class Main {
 
-	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost/";
-
-	static final String USER = "root";
-	static final String PASS = null;	
-	
   public static void main(String[] args) {
-	
-	Connection conn = null;
-	Statement stmt = null;
+
 	try {
-		Class.forName("com.mysql.jdbc.Driver");
+		
+		String username = "user";
+		String password = "password";
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/test", username, password);
 
-		conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		Statement stmt = conn.createStatement();
 
-		stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("select * from inventory");
 
-		String sql = "CREATE DATABASE PREDICTOR";
-		stmt.executeUpdate(sql);
-	}catch(SQLException se){
-		se.printStackTrace();
-	}catch(Exception e){
+		while (rs.next()){
+			System.out.println(rs.getString("name") + ", " + rs.getString("amount"));
+		}
+
+	}catch(SQLException e){
 		e.printStackTrace();
-	}finally{
-		try{
-			if(stmt!=null)
-				stmt.close();
-		}catch(SQLException se2){
-		}
-		try{
-			if(conn!=null)
-				conn.close();
-		}catch(SQLException se){
-			se.printStackTrace();
-		}
 	}
 
-    System.out.println("Hello World");
   }
 }
